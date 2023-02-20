c9851817fbb2bf33a57d759328d03ce2977631bf
Eduardo Souza Rocha <eduardos.rocha17@gmail.com>
2023-02-20 13:19:17 -0300

    Refactor: Extract App to it own file

---
+++ src/app.rs
@@ -0,0 +1,106 @@
+use opengl_graphics::GlGraphics;
+use palette::{Gradient, LinSrgba, Pixel};
+use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
+use piston::Event;
+
+use crate::field::Field;
+use crate::history::History;
+
+pub struct App<const HISTORY_LEN: usize> {
+    /// Field to visualize
+    field: Field,
+    time: f64,
+
+    /// OpenGL drawing backend
+    gl: GlGraphics,
+    history: History<HISTORY_LEN>,
+    pub gradient: Vec<[f32; 4]>,
+}
+
+impl<const HISTORY_LEN: usize> App<HISTORY_LEN> {
+    pub fn new(gl: GlGraphics, field: Field) -> Self {
+        let mut history = History::new();
+        history.spawn();
+
+        let red = LinSrgba::new(1.0, 0.0, 0.0, 1.0);
+        let white = LinSrgba::new(1.0, 1.0, 1.0, 1.0);
+
+        let gradient = Gradient::new([red, white]);
+        let gradient: Vec<[f32; 4]> = gradient
+            .take(HISTORY_LEN)
+            .map(|srgba| srgba.into_format().into_raw())
+            .collect();
+
+        Self {
+            field,
+            time: 0.0,
+            gl,
+            history,
+            gradient,
+        }
+    }
+
+    pub fn renew(self, field: Field) -> Self {
+        Self {
+            field,
+            time: 0.0,
+            gl: self.gl,
+            history: History::new(),
+            gradient: self.gradient,
+        }
+    }
+
+    fn render(&mut self, args: &RenderArgs) {
+        const BACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+        const RADIUS: f64 = 0.002;
+
+        let [width, height] = args.window_size;
+        let side = width.max(height);
+
+        self.gl.draw(args.viewport(), |c, gl| {
+            use graphics::*;
+            // Clear the screen
+            clear(BACK, gl);
+
+            let transform = c
+                .transform
+                .trans(width / 2.0, height / 2.0)
+                .scale(side, side)
+                .scale(0.5, -0.5);
+            //.trans(1.0, -1.0);
+
+            for (i, gen) in self.history.gen_iter().enumerate() {
+                for l in gen {
+                    line(self.gradient[i], RADIUS, l, transform, gl);
+                }
+            }
+        });
+    }
+
+    fn update(&mut self, args: &UpdateArgs) {
+        const NUMBER_OF_SPAWNS: usize = 10;
+        const EXPIRATION_DATE: u32 = 150;
+
+        let dt = args.dt;
+
+        (0..NUMBER_OF_SPAWNS).for_each(|_| self.history.spawn());
+        self.history.expires(EXPIRATION_DATE);
+
+        for p in self.history.data_iter_mut() {
+            let &(x, y) = p.last().unwrap();
+            let (dx, dy) = (self.field)(x, y, self.time);
+            p.push((x + dx * dt, y + dy * dt));
+        }
+        self.time += dt;
+    }
+
+    pub fn handle_envent(&mut self, event: &Event) {
+        if let Some(args) = event.render_args() {
+            self.render(&args);
+        }
+
+        if let Some(args) = event.update_args() {
+            self.update(&args);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,17 @@ extern crate opengl_graphics;
 extern crate palette;
 extern crate piston;
 
+mod app;
 mod field;
 mod history;
 
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{GlGraphics, OpenGL};
-use palette::{Gradient, LinSrgba, Pixel};
 use piston::event_loop::{EventSettings, Events};
-use piston::input::{ButtonEvent, RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
-use piston::Event;
 
-use field::{Field, FIELDS};
-use history::History;
+use crate::app::App;
+use crate::field::FIELDS;
 
 const SIZE: (u32, u32) = (500, 500);
 const N: usize = 50;
@@ -32,12 +30,13 @@ fn main() {
         .unwrap();
 
     let mut field_idx: usize = 0;
-    let mut app = App::new(GlGraphics::new(opengl), FIELDS[field_idx]);
+    let mut app: App<N> = App::new(GlGraphics::new(opengl), FIELDS[field_idx]);
     let mut events = Events::new(EventSettings::new());
 
     // Run visualization
     while let Some(e) = events.next(&mut window) {
         // Change field being visualized
+        use piston::input::ButtonEvent;
         if let Some(args) = e.button_args() {
             use piston::input::Button::Keyboard;
             use piston::keyboard::Key;
@@ -62,104 +61,5 @@ fn main() {
 
         // Hangle event
         app.handle_envent(&e);
-    }
-}
-
-struct App {
-    /// Field to visualize
-    field: Field,
-    time: f64,
-
-    /// OpenGL drawing backend
-    gl: GlGraphics,
-    history: History<N>,
-    gradient: Vec<[f32; 4]>,
-}
-
-impl App {
-    fn new(gl: GlGraphics, field: Field) -> Self {
-        let mut history = History::new();
-        history.spawn();
-
-        let red = LinSrgba::new(1.0, 0.0, 0.0, 1.0);
-        let white = LinSrgba::new(1.0, 1.0, 1.0, 1.0);
-
-        let gradient = Gradient::new([red, white]);
-        let gradient: Vec<[f32; 4]> = gradient
-            .take(N)
-            .map(|srgba| srgba.into_format().into_raw())
-            .collect();
-
-        Self {
-            field,
-            time: 0.0,
-            gl,
-            history,
-            gradient,
-        }
-    }
-
-    fn renew(self, field: Field) -> Self {
-        Self {
-            field,
-            time: 0.0,
-            gl: self.gl,
-            history: History::new(),
-            gradient: self.gradient,
-        }
-    }
-
-    fn render(&mut self, args: &RenderArgs) {
-        const BACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
-        const RADIUS: f64 = 0.002;
-
-        let [width, height] = args.window_size;
-        let side = width.max(height);
-
-        self.gl.draw(args.viewport(), |c, gl| {
-            use graphics::*;
-            // Clear the screen
-            clear(BACK, gl);
-
-            let transform = c
-                .transform
-                .trans(width/2.0, height/2.0)
-                .scale(side, side)
-                .scale(0.5,-0.5);
-                //.trans(1.0, -1.0);
-
-            for (i, gen) in self.history.gen_iter().enumerate() {
-                for l in gen {
-                    line(self.gradient[i], RADIUS, l, transform, gl);
-                }
-            }
-        });
-    }
-
-    fn update(&mut self, args: &UpdateArgs) {
-        const NUMBER_OF_SPAWNS: usize = 10;
-        const EXPIRATION_DATE: u32 = 150;
-
-        let dt = args.dt;
-
-        (0..NUMBER_OF_SPAWNS).for_each(|_| self.history.spawn());
-        self.history.expires(EXPIRATION_DATE);
-
-        for p in self.history.data_iter_mut() {
-            let &(x, y) = p.last().unwrap();
-            let (dx, dy) = (self.field)(x, y, self.time);
-            p.push((x + dx * dt, y + dy * dt));
-        }
-        self.time += dt;
-    }
-
-    fn handle_envent(&mut self, event: &Event) {
-        if let Some(args) = event.render_args() {
-            self.render(&args);
-        }
-
-        if let Some(args) = event.update_args() {
-            self.update(&args);
-        }
     }
 }
